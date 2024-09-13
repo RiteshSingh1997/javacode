07c2ee01801cb27a751398c89c588796b4f57001
RiteshSingh1997 <riteshsingh0689@gmail.com>
2024-09-13 21:10:14 +0530

    While Loop Program

---
+++ Infinity_WhileLoop.java
@@ -0,0 +1,23 @@
+package Collection_Interface;
+
+public class Infinity_WhileLoop 
+{
+
+	public static void main(String[] args) 
+	{
+		int a =10;            // declaration and initialization
+//        while(a>5)
+//        {
+//        	System.out.println(a);
+//        	a++;
+//          }
+        
+        while(a<20)
+        {
+        	System.out.println(a);
+        	a--;
+        }
+
+	}
+
+}
